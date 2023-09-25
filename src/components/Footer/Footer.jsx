ce2e8a66b552660668a9d0eb53815baed54a38f7
Umud-Quliyev <umudquliyev.1806@gmail.com>
2023-09-25 22:09:54 +0400

    =

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,82 +10,170 @@ import {
   AiTwotoneHome,
 } from "react-icons/ai";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import { Accordion, AccordionItem } from "@szhsin/react-accordion";
 
 function Footer() {
   return (
     <footer>
       <div className={style.footer}>
         <div className={style.links}>
-          <h2>QUICK LINKS</h2>
-          <ul>
-            <li>
-              <a href="">About Us</a>
-            </li>
-            <li>
-              <a href="">Faq</a>
-            </li>
-            <li>
-              <a href="">Help</a>
-            </li>
-            <li>
-              <a href="">My Account</a>
-            </li>
-            <li>
-              <a href="">Blog</a>
-            </li>
-            <li>
-              <a href="">Contacts</a>
-            </li>
-          </ul>
+          <div className={style.desktop}>
+            <h2>QUICK LINKS</h2>
+            <ul>
+              <li>
+                <a href="">About Us</a>
+              </li>
+              <li>
+                <a href="">Faq</a>
+              </li>
+              <li>
+                <a href="">Help</a>
+              </li>
+              <li>
+                <a href="">My Account</a>
+              </li>
+              <li>
+                <a href="">Blog</a>
+              </li>
+              <li>
+                <a href="">Contacts</a>
+              </li>
+            </ul>
+          </div>
+          <div className={style.dropdown}>
+            <Accordion>
+              <AccordionItem header="QUICK LINKS">
+                <ul>
+                  <li>
+                    <a href="">About Us</a>
+                  </li>
+                  <li>
+                    <a href="">Faq</a>
+                  </li>
+                  <li>
+                    <a href="">Help</a>
+                  </li>
+                  <li>
+                    <a href="">My Account</a>
+                  </li>
+                  <li>
+                    <a href="">Blog</a>
+                  </li>
+                  <li>
+                    <a href="">Contacts</a>
+                  </li>
+                </ul>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
         <div className={style.categories}>
-          <h2>CATEGORIES</h2>
-          <ul>
-            <li>
-              <a href="">Clothes</a>
-            </li>
-            <li>
-              <a href="">Electronics</a>
-            </li>
-            <li>
-              <a href="">Furniture</a>
-            </li>
-            <li>
-              <a href="">Glasses</a>
-            </li>
-            <li>
-              <a href="">Shoes</a>
-            </li>
-            <li>
-              <a href="">Watches</a>
-            </li>
-          </ul>
+          <div className={style.desktop}>
+            <h2>CATEGORIES</h2>
+            <ul>
+              <li>
+                <a href="">Clothes</a>
+              </li>
+              <li>
+                <a href="">Electronics</a>
+              </li>
+              <li>
+                <a href="">Furniture</a>
+              </li>
+              <li>
+                <a href="">Glasses</a>
+              </li>
+              <li>
+                <a href="">Shoes</a>
+              </li>
+              <li>
+                <a href="">Watches</a>
+              </li>
+            </ul>
+          </div>
+          <div className={style.dropdown}>
+            <Accordion>
+              <AccordionItem header="CATEGORIES">
+                <ul>
+                  <li>
+                    <a href="">Clothes</a>
+                  </li>
+                  <li>
+                    <a href="">Electronics</a>
+                  </li>
+                  <li>
+                    <a href="">Furniture</a>
+                  </li>
+                  <li>
+                    <a href="">Glasses</a>
+                  </li>
+                  <li>
+                    <a href="">Shoes</a>
+                  </li>
+                  <li>
+                    <a href="">Watches</a>
+                  </li>
+                </ul>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
         <div className={style.contacts}>
-          <h2>CONTACTS</h2>
-          <ul>
-            <li>
-              <span>
-                <AiTwotoneHome />
-              </span>
-              <h3>
-                97845 Baker st. 567 <br />
-                Los Angeles - US
-              </h3>
-            </li>
-            <li>
-              <span>
-                <BsFillTelephoneFill />
-              </span>
-              <h3>+94 423-23-221</h3>
-            </li>
-            <li>
-              <span>
-                <AiOutlineMail />
-              </span>
-              <h3>info@allaia.com</h3>
-            </li>
-          </ul>
+          <div className={style.desktop}>
+            <h2>CONTACTS</h2>
+            <ul>
+              <li>
+                <span>
+                  <AiTwotoneHome />
+                </span>
+                <h3>
+                  97845 Baker st. 567 <br />
+                  Los Angeles - US
+                </h3>
+              </li>
+              <li>
+                <span>
+                  <BsFillTelephoneFill />
+                </span>
+                <h3>+94 423-23-221</h3>
+              </li>
+              <li>
+                <span>
+                  <AiOutlineMail />
+                </span>
+                <h3>info@allaia.com</h3>
+              </li>
+            </ul>
+          </div>
+          <div className={style.dropdown}>
+            <Accordion>
+              <AccordionItem header="CONTACTS">
+                <ul>
+                  <li>
+                    <span>
+                      <AiTwotoneHome />
+                    </span>
+                    <h3>
+                      97845 Baker st. 567 <br />
+                      Los Angeles - US
+                    </h3>
+                  </li>
+                  <li>
+                    <span>
+                      <BsFillTelephoneFill />
+                    </span>
+                    <h3>+94 423-23-221</h3>
+                  </li>
+                  <li>
+                    <span>
+                      <AiOutlineMail />
+                    </span>
+                    <h3>info@allaia.com</h3>
+                  </li>
+                </ul>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
         <div className={style.social}>
           <h2>KEEP IN TOUCH</h2>
@@ -113,7 +201,9 @@ function Footer() {
           </div>
         </div>
       </div>
+
       <div className={style.copyright}>
+        <hr />
         <h3>Copyright © 2023 All Rights Reserved.</h3>
       </div>
     </footer>
